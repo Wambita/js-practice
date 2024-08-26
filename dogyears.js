9722ea9b7fbef1f98bc6941a01c514ecebc41651
shfana <wambitafana@gmail.com>
2024-08-26 20:50:36 +0300

    Fix: make functions simpler

--- dogyears.js
+++ dogyears.js
@@ -1,27 +1,20 @@
-const orbitalPeriod = {
-    earth : 1.0,
-    mercury : 0.2408467,
-    venus : 0.61519726,
-    mars : 1.8808158,
-    jupiter : 11.862615,
-    saturn : 29.447498,
-    uranus :84.016846,
-    neptune: 164.79132,
-}
 
 
-function dogYears(planet,  age)   {
 
-    const  secondsInYear = 31557600
-    planet = planet.toLowerCase()
-    let planetYear;
-
-    if (orbitalPeriod.hasOwnProperty(planet)) {
-        planetYear = ((age / secondsInYear) / orbitalPeriod[planet] )* 7
-       return parseFloat(planetYear.toFixed(2))
-    } else {
-        return "Invalid planet name"
+function dogYears(planet,  seconds)   {
+    const orbitalPeriod = {
+        earth : 1.0,
+        mercury : 0.2408467,
+        venus : 0.61519726,
+        mars : 1.8808158,
+        jupiter : 11.862615,
+        saturn : 29.447498,
+        uranus :84.016846,
+        neptune: 164.79132,
     }
+    const  secondsInYear = 31557600
+    const age =  seconds/secondsInYear/orbitalPeriod[planet]*7
+    return  Number(age.toFixed(2))
 }
 
 console.log(dogYears("earth", 1000000000));   // Expected output: Earth years, rounded
