71b85e7381f1a9ca8f697c5f1eb441a4699b16fc
shfana <wambitafana@gmail.com>
2024-08-26 14:00:12 +0300

    Fix: more and less functions

--- more-or-less.js
+++ more-or-less.js
@@ -1,15 +1,16 @@
 function more(value) {
-    return value++
+    return value+1;
 }
 
 function less(value) {
-    return value--
+    return value-1;
 }
 
 function add(num1, num2){
-    return num1 + num2
+    return num1 + num2;
 }
 
 function sub (num1, num2){
-    return num1-num2
+    return num1-num2;
 }
+
