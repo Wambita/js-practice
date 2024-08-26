73046bd07fb7a5dc26af3e50ce45da071f8d5e40
shfana <wambitafana@gmail.com>
2024-08-26 22:46:08 +0300

    Feat: use ternary operator to claculate abs and ispositive

---
+++ abs.js
@@ -0,0 +1,18 @@
+const   isPositive=  (n)  => (n >0   ? true : false)
+
+const abs = (n) => (n>0 ? n  : -n)
+
+// function abs(n) {
+//     if (isPositive(n)){
+//         return n 
+//     } else {
+//         return  -n
+//     }
+// }
+
+
+
+// console.log(isPositive(44))
+// console.log(isPositive(-44))
+// console.log(abs(44))
+// console.log(abs(-44))
