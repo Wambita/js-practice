2f4d5dcffbee033bdf370d3adf4b1e2a57e0eddf
shfana <wambitafana@gmail.com>
2024-08-26 13:56:14 +0300

    Feat functions operators in js

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(value) {
+    return value++
+}
+
+function less(value) {
+    return value--
+}
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+function sub (num1, num2){
+    return num1-num2
+}
