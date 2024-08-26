8670fb751fa3d0788963607e08c51c5fe18e5f3f
shfana <wambitafana@gmail.com>
2024-08-26 15:46:02 +0300

    Feat: add concat function in js

--- concat-str.js
+++ concat-str.js
@@ -2,5 +2,5 @@ function concatStr(a,b) {
     return a +  "" + b;
 }
 
-console.log (concatStr(2,5))
-console.log (concatStr("hello", "world"))
+// console.log (concatStr(2,5))
+// console.log (concatStr("hello", "world"))
