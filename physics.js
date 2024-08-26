9679ed899d6869432817b5eb54dd4d50b6d75f4c
shfana <wambitafana@gmail.com>
2024-08-27 01:49:55 +0300

    Feat: add physics function

---
+++ physics.js
@@ -0,0 +1,33 @@
+function getAcceleration(obj) {
+let a;
+
+if (obj.f !== undefined  && obj.m !== undefined) {
+    //a = f/m
+    a = obj.f/obj.m;
+} else if (obj.Δv !== undefined  && obj.Δt !== undefined) {
+    //a =v/t
+    a = obj.Δv/obj.Δt
+} else if  (obj.d !== undefined && obj.t !== undefined) {
+    //a =d/t
+    a = 2*obj.d/ (obj.t*obj.t)
+}  else {
+    return "impossible"
+}
+
+return a
+
+
+}
+
+
+// obj = {
+//     f: 10,
+//     m: 5,
+//     Δv: 100,
+//     Δt: 50,
+//     t:1,
+//     d: 10
+  
+// }
+
+// console.log(getAcceleration(obj))
