13901dabd2cb7bc94924db74aba281dd9dc9ed17
shfana <wambitafana@gmail.com>
2024-08-29 16:47:47 +0300

    Feat: add index-of function

---
+++ index-of.js
@@ -0,0 +1,40 @@
+function indexOf(arr, val, b = 0) {
+    if (b  > arr.length - 1) {
+        b = 0
+    }
+for (var i = b; i < arr.length; i++) {
+    if (arr[i] === val) {
+        return i
+}
+}
+return -1
+}
+
+
+function lastIndexOf(arr, val, b = 0) {
+    if (b  > arr.length - 1) {
+        b = 0
+    }
+for (var i = arr.length; i >= 0; i--) {
+    if (arr[i] === val) {
+        return i
+}
+}
+return -1
+}
+
+
+function includes(arr, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return true
+    }
+}
+return false
+}
+// console.log(indexOf(['b', 'arr', ' a', 'arr', ], 'arr'))
+// console.log(indexOf(['b', 'arr', ' a', 'arr', ], 'arr', 2))
+// console.log(lastIndexOf(['b', 'arr', ' a', 'arr', ], 'arr'))
+// console.log(lastIndexOf(['b', 'arr', ' a', 'arr', ], 'arr', 2))
+// console.log(includes(['b', 'arr', ' a', 'arr', ], 'arr'))
+// console.log(lastIndexOf(['b', 'arr', ' a', 'arr', ], 'c'))
