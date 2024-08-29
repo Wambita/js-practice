df972936959509eb6120e6808dae708ec9667ce1
shfana <wambitafana@gmail.com>
2024-08-29 17:16:41 +0300

    Feat slicer function

---
+++ slicer.js
@@ -0,0 +1,26 @@
+function slice (input, start, end = input.length) {
+    if (end  > input.length && end < start) {
+        end  = input.length;
+    }
+
+    if (start > input.length && start > end || start < 0) {
+        start = 0
+    }
+if (Array.isArray(input)) {
+    let res = [];
+    for (let i = start; i < end; i++) {
+        res.push(input[i]);
+    }
+    return res;
+}
+if (typeof input === 'string') {
+    let res = ''
+    for(let i=start; i<=end; i++){
+        res += input[i]
+    }
+    return res;
+}
+}
+
+console.log(slice([1, 2, 3, 4, 5], 2, 4)); // [3, 4]
+console.log(slice('Hello, World!', 6, 11)); // 'World!'
