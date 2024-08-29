c61b92fabcbc8277712104318fe1351fbeedbf5a
shfana <wambitafana@gmail.com>
2024-08-29 18:07:24 +0300

    Fix: error handling

--- keep-cut.js
+++ keep-cut.js
@@ -1,19 +1,19 @@
 
 //returns the string with the first 2 characters removed.
-const  cutFirst = (string) => (string.length >= 2 ) ? '': string.substring(2, string.length)
+const  cutFirst = (string) => (string.length <= 2 ) ? '': string.substring(2, string.length)
 
 //returns the string with the last 2 characters removed.
-const  cutLast = (string) =>  (string.length >= 2 ) ? '': string.substring(0, string.length-2)
+const  cutLast = (string) =>  (string.length <= 2 ) ? '': string.substring(0, string.length-2)
 
 //returns the string with the first 2 characters and the last 2 characters removed.
-const  cutFirstLast = (string) =>  (string.length >= 2 ) ? '': string.substring(2, string.length-2)
+const  cutFirstLast = (string) =>  (string.length <= 2 ) ? '': string.substring(2, string.length-2)
 
 //keepFirst returns only the first 2 characters.
-const  keepFirst = (string) =>  (string.length >= 2 ) ?  string :string.substring(0,2)
+const  keepFirst = (string) =>  (string.length <= 2 ) ?  string :string.substring(0,2)
 
 
 //keepLast returns only the last 2 characters
-const  keepLast = (string) => (string.length >= 2 ) ?  string: string.substring(string.length-2,string.length)
+const  keepLast = (string) => (string.length <= 2 ) ?  string: string.substring(string.length-2,string.length)
 
 //keppFirstLast returns the first 2 characters, and the last 2 characters.
 function keepFirstLast(string) {
@@ -22,9 +22,9 @@ function keepFirstLast(string) {
 }
 
 
- //console.log(cutFirst('abcdef'))
+//  console.log(cutFirst('abcdef'))
 // console.log(cutLast("taeyon"))
-//console.log(cutFirstLast("af"))
+// console.log(cutFirstLast("af"))
 // console.log(keepFirst("taeyon"))
 // console.log(keepLast("taeyon"))
 // console.log(keepFirstLast("taeyon"))
