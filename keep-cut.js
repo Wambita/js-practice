d7488f01e7f3a16c50dfc0104d64cb9f12ed1fa9
shfana <wambitafana@gmail.com>
2024-08-29 18:18:03 +0300

    Refactor: keepfirstlast and cutfirst function

--- keep-cut.js
+++ keep-cut.js
@@ -1,6 +1,6 @@
 
 //returns the string with the first 2 characters removed.
-const  cutFirst = (string) => (string.length <= 2 ) ? '': string.substring(2, string.length)
+const  cutFirst = (string) => (string.length <= 2 ) ? '': string.substring(2)
 
 //returns the string with the last 2 characters removed.
 const  cutLast = (string) =>  (string.length <= 2 ) ? '': string.substring(0, string.length-2)
@@ -16,15 +16,15 @@ const  keepFirst = (string) =>  (string.length <= 2 ) ?  string :string.substrin
 const  keepLast = (string) => (string.length <= 2 ) ?  string: string.substring(string.length-2,string.length)
 
 //keppFirstLast returns the first 2 characters, and the last 2 characters.
-function keepFirstLast(string) {
-    if (string.length <= 4) return string  // if string length is less than 2, return an empty string.
-    return  keepFirst(string)+ keepLast(string)  // concatenate the first two and last two characters.
-}
+const  keepFirstLast = (string)  =>  (string.length <= 4) ? string :  keepFirst(string)+ keepLast(string) 
+ // if string length is less than 2, return an empty string.
+  // concatenate the first two and last two characters.
 
 
-//  console.log(cutFirst('abcdef'))
-// console.log(cutLast("taeyon"))
-// console.log(cutFirstLast("af"))
-// console.log(keepFirst("taeyon"))
-// console.log(keepLast("taeyon"))
-// console.log(keepFirstLast("taeyon"))
+
+ console.log(cutFirst('abcdef'))
+console.log(cutLast("taeyon"))
+console.log(cutFirstLast("af"))
+console.log(keepFirst("taeyon"))
+console.log(keepLast("taeyon"))
+console.log(keepFirstLast("taeyon"))
