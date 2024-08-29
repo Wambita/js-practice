0f13aa3f2e10c92d5b6e8b98adfde81694cfc606
shfana <wambitafana@gmail.com>
2024-08-29 15:49:41 +0300

    Fix divide function

--- elementary.js
+++ elementary.js
@@ -18,11 +18,17 @@ function multiply  (a,b ){
 
 function divide (a,b) {
     let count = 0 ;
+if (a === 0 ){
+    return 0
+} else if (b === 0) {
+    return "Error: Division by zero"
+}
 
+const allneg = (a < 0) === (b <0)
     if (b > a) {
         return 0
     }
-    for (var i=a-1; i >= 0; i-=b) {
+    for (var i=a-1; i >= b; i-=b) {
         a = a - b
         count ++
     }
@@ -40,6 +46,6 @@ function modulo(a,b){
     return a
 }
 
-// console.log(multiply(123, -22))
-// console.log(divide(34, 78))
+console.log(multiply(123, -22))
+ console.log(divide(78, 34))
 
