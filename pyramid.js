34b144d7959e25eec782f19a87de608e3ac48761
shfana <wambitafana@gmail.com>
2024-08-30 19:00:37 +0300

    Feat: add pyramid function

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,10 @@
 function pyramid(char, depth) {
+    if (depth === 0) {
+        return ''
+    }
     let res  = ''
     for (var i = 0; i < depth; i++) {
-        let spaces = ' '.repeat(depth -i -1)
+    let spaces = ' '.repeat(depth -i-1 )
       let chars = char.repeat(2 * i + 1) 
         res += spaces + chars
         if ( i !== depth-1){
@@ -11,4 +14,4 @@ function pyramid(char, depth) {
 return res
 }
 
-//console.log(pyramid('*', 6))
+//console.log(pyramid('*', 12))
