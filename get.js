57dd01a2751d760b5dd51ea33fec6cb4848aac9e
shfana <wambitafana@gmail.com>
2024-08-30 18:14:43 +0300

    Feat: naming issues

--- get.js
+++ get.js
@@ -1,12 +1,12 @@
 function get(src, path) {
-    const path = path.split('.')
+    const paths = path.split('.')
     let res = src 
 
-    for (let i = 0; i < path.length; i++) {
+    for (let i = 0; i < paths.length; i++) {
         if (res === undefined || res === null  ) {
         return undefined
     }
-    res = res[path[i]]
+    res = res[paths[i]]
 }
 return res
 }
