2dd26ae175e021f535dab7b47e8a5e766b0ae531
shfana <wambitafana@gmail.com>
2024-08-30 17:58:56 +0300

    Fix loop

--- nasa.js
+++ nasa.js
@@ -1,6 +1,6 @@
 function nasa(N) {
     let res = ''
-    for (let i = 1; i < N; i++) {
+    for (let i = 1; i <= N; i++) {
         if (i %3  === 0 && i %5 === 0) {
             res += 'NASA'
         } else if (i %3 === 0 && i %5 !== 0) {
