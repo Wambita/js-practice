d6e66a30fb985a6775ff930eb7a5b359fde4e80a
shfana <wambitafana@gmail.com>
2024-08-30 18:38:20 +0300

    Feat: add fix newline issue

--- triangle.js
+++ triangle.js
@@ -1,9 +1,24 @@
 function triangle(char, depth) {
 let res = ''
 for (var i = 1; i<=depth; i++) {
-    res += char.repeat(i) + '\n'
-    // res += '\n'
+    res += char.repeat(i) 
+    if (i !== depth) {
+        res += '\n'
+    }
 }
 return res
 }
-//console.log(triangle("*", 5))
+// console.log(triangle("*", 5))
+// //Recursion function
+
+// function triangle(char, depth) {
+//     //base 
+//     if (depth <= 0){
+//         return ''
+//     }
+// //recursion function
+//     const smallTriangle = triangle(char, depth -1)
+//     const currLine =  char.repeat(depth)
+
+//     return smallTriangle +  currLine + '\n'
+// }
