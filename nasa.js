07fd66058c625ec3c0d831ae2fcd1c324792623b
shfana <wambitafana@gmail.com>
2024-08-30 17:52:54 +0300

    Feat: add nasa js

---
+++ nasa.js
@@ -0,0 +1,19 @@
+function nasa(N) {
+    let res = ''
+    for (let i = 0; i < N; i++) {
+        if (i %3  === 0 && i %5 === 0) {
+            res += 'NASA'
+        } else if (i %3 === 0 && i %5 !== 0) {
+            res += 'NA'
+        } else if (i %3 !== 0 && i %5  === 0){
+            res += 'SA'
+        } else {
+            res += i
+        }
+
+        if (i  !== N) {
+            res += ' '
+        }
+    }
+    return res
+}
