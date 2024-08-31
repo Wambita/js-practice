8f85437ec8d9d0fc8ab4e5ae406d56366b709fb5
Wambita <wambitafana@gmail.com>
2024-08-31 17:42:31 +0300

    feat add regex

--- same-amount.js
+++ same-amount.js
@@ -3,8 +3,8 @@ function sameAmount(str, regex1, regex2) {
     regex1 = new RegExp(regex1,"g");
     regex2 = new RegExp(regex2,"g");
 
-    const match1 = str.match(regex1);
-    const match2 = str.match(regex2);
+    const match1 = str.match(regex1) || []
+    const match2 = str.match(regex2) || []
     return match1.length === match2.length;
 }
 
