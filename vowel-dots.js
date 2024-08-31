9ca2aebf041b6f78e97d4bcd4cd7350ef2a4a9de
Wambita <wambitafana@gmail.com>
2024-08-31 17:23:42 +0300

    feat add vowel dots

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,10 +1,10 @@
-const vow = /[aeiou]/gi
+const vowels = /[aeiou]/gi
 
 function vowelDots(str) {
     let result = '';
 
     for (var i=0; i<str.length; i++) {
-        if (vow.test(str[i])) {
+        if (vowels.test(str[i])) {
             result += str[i] + '.';
         } else {
             result += str[i];
