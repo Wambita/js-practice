2d34f9f951f71337c119b8f5a9df509c774a71b4
Wambita <wambitafana@gmail.com>
2024-09-04 02:02:51 +0300

    feat add date functions

---
+++ date-is.js
@@ -0,0 +1,41 @@
+//isValid: accepts a Date, and returns false if the Date is invalid.
+function isValid(date){
+    return(date instanceof Date && !isNaN(date.getTime())) || (typeof date === 'number' && !isNaN(date) && date >= 0)
+}
+
+// console.log(isValid(new Date('2020-01-01'))); // true
+// console.log(isValid(new Date('20203-13-01'))); //false
+
+//isAfter: accepts two Date arguments, and returns true if the first is greater then the second.
+function isAfter(date1, date2){
+    const d1 = date1 instanceof Date ? date1 : new Date(date1)
+    const d2 = date2 instanceof Date? date2 : new Date(date2)
+
+    return isValid(d1) && isValid(d2) && d1.getTime() > d2.getTime()
+}
+//console.log(isAfter('2020-01-03', '2020-01-02')); // false,
+
+//isBefore: accepts two Date arguments, and returns true if the second is greater than the first.
+function isBefore(date1, date2){
+    const d1 = date1 instanceof Date ? date1 : new Date(date1)
+    const d2 = date2 instanceof Date? date2 : new Date(date2)
+
+    return isValid(d1) && isValid(d2) && d2.getTime() > d1.getTime()
+
+}
+//console.log(isBefore('2020-01-03', '2020-01-02')); // false,
+
+//isFuture: accepts a Date, and returns true if the Date is valid, and is after than the present date.
+function isFuture(date){
+    const d = date instanceof Date ? date : new Date(date)
+    return isValid(d) && isAfter(d, new Date())
+}
+
+//console.log(isFuture('2025-01-01')); // true,
+
+//isPast: accepts a Date, and returns true if the Date is valid, and is before the present date.
+function isPast(date){
+    const d = date instanceof Date? date : new Date(date)
+    return isValid(d) && isBefore(d, new Date())
+}
+//console.log(isPast('2020-01-01')); // true,
