97e4075775f9dcac96c921d09c2e19a89d092c1b
shfana <wambitafana@gmail.com>
2024-09-04 10:37:14 +0300

    Add comments to code

--- same-amount.js
+++ same-amount.js
@@ -1,15 +1,27 @@
 //same amount that takes three arguments: a string, and 2 regular expressions. Your function should return a boolean.
-function sameAmount(str, regex1, regex2) {
-    regex1 = new RegExp(regex1,"g");
-    regex2 = new RegExp(regex2,"g");
+function sameAmount(str, regex1, regex2){
+    //change the reegx args  to regex obects
+    regex1  = new RegExp(regex1, 'g')
+    regex2 =  new RegExp(regex2, 'g')
 
-    const match1 = str.match(regex1) || []
-    const match2 = str.match(regex2) || []
-    return match1.length === match2.length;
+    //match the strings with the regexs
+    match1 = str.match(regex1) || [];
+    match2 = str.match(regex2) || []
+
+    return match1.length === match2.length
 }
 
+// // Example 1: Check if a string has an equal number of vowels and consonants
+// const exampleString = "hello";
+// const vowelsPattern = "[aeiou]";
+// const consonantsPattern = "[bcdfghjklmnpqrstvwxyz]";
 
-// const word = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
-//  ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
-// console.log(sameAmount('12345', /[54321]/g, /[12345]/g))
-// console.log(sameAmount(data, /qqqq /, /qqqqqqq/))
+// const result1 = sameAmount(exampleString, vowelsPattern, consonantsPattern);
+// console.log(result1);  // Output: false
+
+// const exampleString2 = "xoxoxo";
+// const xPattern = "x";
+// const oPattern = "o";
+
+// const result2 = sameAmount(exampleString2, xPattern, oPattern);
+// console.log(result2);  // Output: true
