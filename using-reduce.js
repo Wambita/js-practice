43d0618a01ecd0267786f6d0a4135fe396cc7ac9
shfana <wambitafana@gmail.com>
2024-09-04 16:16:56 +0300

    Feat: add reduce function

--- using-reduce.js
+++ using-reduce.js
@@ -1,20 +1,20 @@
 //adder: accepts an array of numbers, and returns the sum as a number.
-function adder(arr) {
-    const initialVal = 0
+function adder(arr, additional =0) {
+    const initialVal =  additional
   const sum = arr.reduce(( acc,  current) => acc+ current , initialVal )
     return sum
 }
 
 //sumOrMul: accepts an array of numbers and adds or multiplies its elements depending on whether the element is odd or even. Even = multiply. Odd = add.
-function sumOrMul(arr) {
-    const initialVal = arr[0] % 2 === 0? 1 : 0
+function sumOrMul(arr, additional = 0) {
+    const initialVal = additional || arr[0] % 2 === 0? 1 : 0
     sumOrMul = arr.reduce((acc,  current) => (current % 2 === 0? acc * current : acc + current), initialVal )
     return sumOrMul
 }
 
 //funcExec: accepts an array of functions and executes them using reduce, returning the result.
-function funcExec(arr) {
-    const result = arr.reduce((acc, func) => func(acc), 0)
+function funcExec(arr, initialVal = 0) {
+    const result = arr.reduce((acc, func) => func(acc),  initialVal)
     return result
 }
 
