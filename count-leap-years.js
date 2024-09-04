bff693a4c8d4b983eefebc9e89217d17e6e0fde6
Wambita <wambitafana@gmail.com>
2024-09-04 03:38:03 +0300

    Fix: add count leap years function

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,5 +1,6 @@
 //return the number of leap years to have taken place since the year 1.
-function countLeapYears(year) {
+function countLeapYears(date) {
+    const year = date.getFullYear();
     let count = 0;
     for (let i = 1; i < year; i++) {
         if (isLeapYear(i)) {
@@ -12,4 +13,4 @@ function countLeapYears(year) {
 function isLeapYear(year) {
     return (year % 4 === 0 && year % 100!== 0) || year % 400 === 0
 }
-//console.log(countLeapYears(2020));
+//console.log(countLeapYears(new Date('1664-08-09')));
