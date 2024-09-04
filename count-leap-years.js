5ba8f04209b5f555207899e4375ba718eb959a89
Wambita <wambitafana@gmail.com>
2024-09-04 03:15:37 +0300

    Feat: add count leap years function

---
+++ count-leap-years.js
@@ -0,0 +1,15 @@
+//return the number of leap years to have taken place since the year 1.
+function countLeapYears(year) {
+    let count = 0;
+    for (let i = 1; i <= year; i++) {
+        if (isLeapYear(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100!== 0) || year % 400 === 0
+}
+//console.log(countLeapYears(2020));
