3dcb2a4c283f8264a6caff52a1907fe87e471b3f
shfana <wambitafana@gmail.com>
2024-09-05 22:56:23 +0300

    Feat: add filter.js

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@
 function filter(arr, func){
     let res = []
     for (let i=0; i<arr.length; i++){
-        if (func(arr[i])){
+        if (func(arr[i],i,arr)){
             res.push(arr[i])
         }
     }
