644d64f392fcdf5629a9c6fda085dba06e56f6b1
shfana <wambitafana@gmail.com>
2024-09-06 14:36:43 +0300

    Feat: add get them all functionality

--- get-them-all.js
+++ get-them-all.js
@@ -1,6 +1,6 @@
 //getArchitects: find all  a tags and check those with class architects and non architects
 export const getArchitects = () =>[document.querySelector("body a"),
-documen.querySelector("body span")]
+document.querySelector("body span")]
 
 //getClassical : filters the architects further to find those in the classical class and those who arent
 export const getClassical = () =>[document.querySelectorAll("a.classical"), 
