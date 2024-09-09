8472375346bab9962f8a27e8a68460422c0025b7
shfana <wambitafana@gmail.com>
2024-09-09 10:38:03 +0300

    Feat: add shades of blue

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,6 +1,18 @@
-import  {colors} form "./fifty"
- 
+import  {colors} from  './fifty-shades-of-cold.data.js'
 
-export function  generateClasses(){
 
+export function generateClasses() {
+    const head =  document.getElementByTagName("head")[0]
+    const  style = document.createElement("style")
+    colors.forEach((color) =>{
+        style.innerHTML += `.${color}{background: $color};}`
+    })
+    console.log(style.innerHTML)
+    head.appendChild(style)
+} 
+
+export function  choseShade(shade) {
+    document.querySelectorAll("div").forEach((div) => {
+        div.className = shade
+    })
 }
