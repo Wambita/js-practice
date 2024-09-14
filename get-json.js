733701c9cd82eca9884d5ba819ff1d80d9aaf295
Wambita <wambitafana@gmail.com>
2024-09-14 06:05:51 +0300

    Feat: add get json, complex async functions

---
+++ get-json.js
@@ -0,0 +1,16 @@
+async function getJson(path, params){
+    let formatpar = ""
+    for (const [k, v] of Object.entries(params)) {
+        formatpar += `${k}=${v}&.replaceAll(" ", "+")`
+    }
+    let url  = `${path}?${formatpar.slice(0, -1)}`
+
+    return await fetch(url).then(response => {
+        if(!response.ok) throw new Error(response.statusText)
+            return response.json()
+    
+    }).then(json => {
+        if(json["data"])return json["data"]
+        if(json["error"]) throw new Error(json["error"])
+    })
+    }
