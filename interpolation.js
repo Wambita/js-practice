ac19aeb5b0613c519915545d9ef6ebf08c5959c6
Wambita <wambitafana@gmail.com>
2024-09-14 04:13:55 +0300

    Fix: add result function

--- interpolation.js
+++ interpolation.js
@@ -1,12 +1,18 @@
 function interpolation(step, start, end , callback, duration){
     let distance = (end - start) / step;
     const interval = duration / step;
+    let res = []
 
     for (let i = 0; i < step; i++) {
         setTimeout(()=>{
             const x = start + (distance * i);
             const y = interval * (i+1)
+            res.push([x, y])
             callback([x, y])
+
+            if (i === step - 1) {
+                callback(res)
+            }
         }, interval * i);
 }
 }
