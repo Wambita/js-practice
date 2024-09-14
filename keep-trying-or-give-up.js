2eef0842c1bc6fe48ef7952b0eab61868e220712
Wambita <wambitafana@gmail.com>
2024-09-14 04:53:26 +0300

    Feat: add keep trying function

---
+++ keep-trying-or-give-up.js
@@ -0,0 +1,15 @@
+function retry(count, callback){
+    return async function(...args){
+    let attempts = 0
+
+    while(attempts <= count){
+        try{
+            return await callback(...args)
+        } catch(error){
+            attempts++
+            if(attempts > count){
+                throw new Error('timeout')            }
+        }
+    }
+}
+}
