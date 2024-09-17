dc4f00dc2f272f31293dcf810f671118bd0ee774
Wambita <wambitafana@gmail.com>
2024-09-17 21:50:45 +0300

    Feat: script to write result from veryDisco function to a txt file

---
+++ verydisco-forever.mjs
@@ -0,0 +1,34 @@
+//script to write result from veryDisco function to a txt file 
+
+import {writeFile } from 'fs/promises'
+
+//disco function
+function veryDisco(word){
+    const mid = Math.ceil(word.length/2)
+    return word.slice(mid) + word.slice(0, mid)
+}
+
+
+//process input
+function inputDisco(input){
+    return input.split(' ').map(veryDisco).join(' ')
+}
+ async function writeToFile(string){
+    try {
+        await writeFile('verydisco-forever.txt', string)
+        console.log('result written to verydisco-forever.txt')
+    } catch (error) {
+        console.error('Error writing to file:', error)
+        process.exit(1)
+    }
+ }
+
+ const input = process.argv[2]
+
+ if(!input){
+    console.log('Please provide an input string.')
+    process.exit(1)
+}
+
+const result = inputDisco(input)
+console.log(result)
